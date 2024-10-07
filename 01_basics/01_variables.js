8b51cac59f49a98b9ecbb87220f69eba80f60a9f
Shoyeb Chaudhari <159788954+ShoyebChaudhari45@users.noreply.github.com>
2024-10-07 06:25:19 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId =14453
+let accountEmail= "shoyeb@google.com"
+var accountPassword = "123"
+accountCity = "Delhi"
+
+// accountId =2
+ accountEmail= "shoyeb@google.com"
+ accountPassword = "23"
+accountCity = "Banglore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+//datatypes 
+let name="shoyeb"//string
+let age = 18 //numbers
+let isLoggedin = false // bolean
+//number
+//bigint
+//string
+//boolean => true/false
+//null 
+//undefined 
+//symbol
+
+console.log(typeof "shoyeb");
+console.log(typeof undefined)
+console.log(typeof null)
